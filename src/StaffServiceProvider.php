595aab337e1eddecaf763f36a1aa8ef25ba71b38
GIS <dev@gis4biz.ru>
2022-10-28 14:40:09 +0300

    initial commit

---
+++ src/StaffServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Notabenedev\SiteStaff;
+
+use Illuminate\Support\ServiceProvider;
+
+class StaffServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/config/site-staff.php
@@ -0,0 +1,7 @@
+<?php
+return [
+
+    "sitePackageName" => "Специалисты",
+    "siteDepartmentName" => "Структура",
+    "siteStaffName" => "Специалисты",
+];
